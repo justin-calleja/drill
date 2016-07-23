bba42a53c53289b2dd184fe695ebabd788a24900
Justin Calleja <calleja.justin@gmail.com>
2016-07-23 19:53:15 +0200

    Sorted listing of commands

---
+++ src/commands/index.js
@@ -0,0 +1,71 @@
+var install = require('./install');
+var gen = require('./gen');
+var lsconf = require('./lsconf');
+var rconf = require('./rconf');
+var econf = require('./econf');
+var pathCmd = require('./path');
+var lsmaterials = require('./lsmaterials');
+
+module.exports = [
+  {
+    txt: 'install <url> [name] [head]',
+    desc: 'installs material from given url',
+    opts: {
+      url: {
+        alias: 'u',
+        describe: 'The url to use to install into the head materials path'
+      },
+      name: {
+        alias: 'n',
+        describe: 'The name to clone to'
+      },
+      head: {
+        alias: 'h',
+        describe: 'Use the head materials path to install into even if there are multiple materials paths'
+      }
+    },
+    fun: install
+  },
+  {
+    txt: 'gen [no-launch-editor]',
+    desc: 'generates a new drill in the workspace',
+    opts: {
+      'no-launch-editor': {
+        alias: 'n',
+        describe: 'Used to skip launching editor in workspace',
+        'default': false
+      }
+    },
+    fun: gen
+  },
+  {
+    txt: 'lsconf',
+    desc: 'list configuration',
+    opts: {},
+    fun: lsconf
+  },
+  {
+    txt: 'rconf',
+    desc: 'resets configuration to default',
+    opts: {},
+    fun: rconf
+  },
+  {
+    txt: 'econf',
+    desc: 'edit configuration',
+    opts: {},
+    fun: econf
+  },
+  {
+    txt: 'path',
+    desc: 'prints drill directory path',
+    opts: {},
+    fun: pathCmd
+  },
+  {
+    txt: 'lsmaterials',
+    desc: 'list available materials',
+    opts: {},
+    fun: lsmaterials
+  }
+];
--- src/index.js
+++ src/index.js
@@ -2,42 +2,19 @@
 
 require('./setup')();
 
-var gen = require('./commands/gen');
-var install = require('./commands/install');
-var lsconf = require('./commands/lsconf');
-var rconf = require('./commands/rconf');
-var econf = require('./commands/econf');
-var pathCmd = require('./commands/path');
-var lsmaterials = require('./commands/lsmaterials');
+var compare = require('./utils/compare');
+var commands = require('./commands');
+var sortedCommands = commands.sort((a, b) => compare(a.txt, b.txt));
 
-require('yargs')
+function addCommands(yargsConf, cmds) {
+  cmds.forEach(cmd => {
+    yargsConf.command(cmd.txt, cmd.desc, cmd.opts, cmd.fun);
+  });
+  return yargsConf;
+}
+
+addCommands(require('yargs'), sortedCommands)
   .usage('$0 <cmd>')
-  .command('install <url> [name] [head]', 'installs material from given url', {
-    url: {
-      alias: 'u',
-      describe: 'The url to use to install into the head materials path'
-    },
-    name: {
-      alias: 'n',
-      describe: 'The name to clone to'
-    },
-    head: {
-      alias: 'h',
-      describe: 'Use the head materials path to install into even if there are multiple materials paths'
-    }
-  }, install)
-  .command('gen [no-launch-editor]', 'generates a new drill in the workspace', {
-    'no-launch-editor': {
-      alias: 'n',
-      describe: 'Used to skip launching editor in workspace',
-      'default': false
-    }
-  }, gen)
-  .command('lsconf', 'list configuration', {}, lsconf)
-  .command('rconf', 'resets config to default', rconf)
-  .command('econf', 'edit config', econf)
-  .command('path', 'prints drill directory path', pathCmd)
-  .command('lsmaterials', 'list available materials', lsmaterials)
   .required( 1, 'At least one command is required to operate drill')
   .help()
   // .epilog( 'Link to submit issues / feature requests?')
