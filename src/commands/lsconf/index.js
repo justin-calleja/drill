80dd8576f001b191b59c69e87beda5d61cd7db54
Justin Calleja <calleja.justin@gmail.com>
2016-07-25 00:39:47 +0200

    lsconf is not hard coded anymore

--- src/commands/lsconf/index.js
+++ src/commands/lsconf/index.js
@@ -1,10 +1,10 @@
 var nconf = require('nconf');
 var columnify = require('columnify');
+var confKeys = require('@justinc/drill-conf').confKeys;
 
 module.exports = function _lsconf(_argv) {
-  console.log(columnify({
-    'workspace.path': nconf.get('workspace.path'),
-    'db.path': nconf.get('db.path'),
-    'materials.paths': nconf.get('materials.paths')
-  }));
+  console.log(columnify(confKeys.reduce((acc, val) => {
+    acc[val] = nconf.get(val);
+    return acc;
+  }, {})));
 };
